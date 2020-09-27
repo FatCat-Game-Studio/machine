c19fef03d37b68979b11f9b826388799902de1ae
Karla Agraz <karla.agraz@gmail.com>
2020-09-26 21:28:14 -0500

    cards actualizadas, falta seguir editando los estilos, actualicé el footer y header de las otras páginas, elimine estilos que ya no funcionaban

--- js/clients.js
+++ js/clients.js
@@ -1,16 +1,17 @@
-$('.brand-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    autoplay:true,
+$(".brands").owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
     responsive:{
         0:{
             items:1
         },
         600:{
-            items:3
+            items:2
         },
         1000:{
-            items:5
+            items:3
         }
     }
-})
+});
